4f14f251115e7ebe6e00363a19189488680729f4
jane742 <janeteslya@gmail.com>
2026-05-31 19:32:48 +0300

    Fix missing URL facade import

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL; // Обов'язково додай цей рядок зверху!
 class AppServiceProvider extends ServiceProvider
 {
     /**
